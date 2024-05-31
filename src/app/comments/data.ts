8132d56639a1679a72b25ddad78c865f30c6c93c
Saputra <saputra.uta50@gmail.com>
2024-05-31 14:27:05 +0800

    First commit

---
+++ src/app/comments/data.ts
@@ -0,0 +1,23 @@
+export const comments = [
+    {
+      "id": 1,
+      "comment": "This product is amazing! Highly recommend it."
+    },
+    {
+      "id": 2,
+      "comment": "Not satisfied with the quality. Expected better."
+    },
+    {
+      "id": 3,
+      "comment": "Great value for the price. Will buy again."
+    },
+    {
+      "id": 4,
+      "comment": "Average product. Nothing special."
+    },
+    {
+      "id": 5,
+      "comment": "Fast shipping and excellent customer service."
+    }
+  ]
+  
