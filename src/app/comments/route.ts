626aa9300bc71e84d96ffe33acf8ff874569b2d7
Saputra <saputra.uta50@gmail.com>
2024-05-31 14:33:55 +0800

    POST Handler

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,5 +1,21 @@
+import { comment } from "postcss";
 import { comments } from "./data";
 
 export async function GET() {
     return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+    const body = await request.json();
+    const newComment = {
+        id: comments.length + 1,
+        comment: body.text,
+    }
+    comments.push(newComment);
+    return new Response(JSON.stringify(newComment), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        status: 201
+    })
 }
