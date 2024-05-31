bdb62f5b699335882dd8ffc5804756d137e3b418
Saputra <saputra.uta50@gmail.com>
2024-05-31 15:19:55 +0800

    Redirecting if comment not found in detail comment

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,7 +1,11 @@
 import { comments } from "../data";
+import { redirect } from "next/navigation";
 
 export async function GET(_request: Request, { params }: { params: { id: string } }) {
     const comment = comments.find((c) => c.id === parseInt(params.id));
+    if (!comment) {
+        redirect("/comments");
+    }
     return Response.json(comment);
 }
 
