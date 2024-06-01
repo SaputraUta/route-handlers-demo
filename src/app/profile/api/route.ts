ddc7a1437e4f304d43bf1e2147bbfb881d5f7391
Saputra <saputra.uta50@gmail.com>
2024-06-01 12:38:28 +0800

    Headers & Cookies

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,22 @@
-export async function GET() {
-    return new Response("Hello profile");
+import { headers, cookies } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request : NextRequest) {
+    const requestHeaders = new Headers(request.headers)
+    const headerList = headers();
+
+    const theme = request.cookies.get("theme");
+    console.log(theme);
+
+    const theme2 = cookies().get("theme");
+    console.log(theme2);
+
+    console.log(requestHeaders.get("Authorization"))
+    console.log(headerList.get("Authorization"))
+    return new Response("<h1>Hello profile</h1>", {
+        headers: {
+            "Content-Type": "text/html",
+            "Set-Cookie" : "theme=dark"
+        },
+    });
 }
